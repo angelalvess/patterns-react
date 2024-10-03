7d313391dc268b50c6c242ce1d229c3868d684ef
angelalves <angeltrashangel@gmail.com>
2024-10-03 02:30:35 -0300

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Widget from "./components/widget";
+
+function App() {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center">
+      <Widget />
+    </div>
+  );
+}
+
+export default App;
